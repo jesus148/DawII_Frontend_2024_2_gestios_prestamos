86dd20b1027238f723dbbaf38d3366b9079bfbcc
DanielPRe15 <dani123pre@gmail.com>
2024-06-04 16:51:21 -0500

    Solucion Front Registro Grupo

--- src/app/components/agregar-grupo/agregar-grupo.component.ts
+++ src/app/components/agregar-grupo/agregar-grupo.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 import { AppMaterialModule } from '../../app.material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../../menu/menu.component';
+import { Grupo } from '../../models/grupo.model';
+import { GrupoService } from '../../services/grupo.service';
+import { UtilService } from '../../services/util.service';
+import { TokenService } from '../../security/token.service';
+import { map } from 'rxjs';
+import { Ubigeo } from '../../models/ubigeo.model';
+import { Usuario } from '../../models/usuario.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-agregar-grupo',
@@ -12,5 +20,131 @@ import { MenuComponent } from '../../menu/menu.component';
   styleUrl: './agregar-grupo.component.css'
 })
 export class AgregarGrupoComponent {
+
+  grupo: Grupo = {
+    descripcion: "",
+    ubigeo:{
+      idUbigeo:-1,
+      departamento:"-1",
+      provincia:"-1",
+      distrito:"",
+    },
+    usuarioLider: {
+      idUsuario: -1
+    },
+    usuarioRegistro: {
+        idUsuario: -1
+    },
+    usuarioActualiza: {
+        idUsuario: -1
+    },  
+  
+  }
+
+  formRegistrar = this.formBuilder.group({
+    validaDescripcion: ['', [Validators.required, Validators.pattern('[a-zA-Z ]{3,30}')], this.validaDescripcion.bind(this)],
+    validaDepartamento: ['', [Validators.min(1)]],
+    validaProvincia: ['', [Validators.min(1)]],
+    validaDistrito: ['', [Validators.min(1)]],
+    validaPrestatario: ['', [Validators.min(1)]],
+  });
+
+  //listas de ubigeo 
+  departamentos : string[] = [];
+  provincias : string[] = [];
+  distritos: Ubigeo[] = [];
+
+  //lista de Jefe de Prestamistas
+  lstJefePrestamista: Usuario[] = [];
+    
+  //usuario en sesion
+  objUsuario: Usuario = {};
+
+  constructor(private grupoService: GrupoService,
+    private utilService: UtilService,
+    private tokenService: TokenService,
+    private formBuilder: FormBuilder) {
+console.log(">>> constructor  >>> ");
+} 
+  ngOnInit(){
+    console.log(">>> OnInit [inicio]");
+    this.utilService.listarDepartamento().subscribe(
+      x => this.departamentos = x
+    );
+    this.utilService.listaJefePrestamistaTotales().subscribe(
+      x => this.lstJefePrestamista = x
+    );
+    this.objUsuario.idUsuario = this.tokenService.getUserId();
+        console.log(">>> OnInit >>> " + this.departamentos);
+        console.log(">>> OnInit >>> " + this.lstJefePrestamista);
+        console.log(">>> OnInit [fin]"); 
+
+  }
+
+
+registrarGrupo(){
+      console.log(">>> registraGrupo [inicio] ");
+      this.grupo.usuarioActualiza = this.objUsuario;
+      this.grupo.usuarioRegistro = this.objUsuario;
+      console.log(">>> registraGrupo [inicio] " + this.grupo);
+      console.log(this.grupo);
+
+      this.grupoService.registrarGrupo(this.grupo).subscribe(
+        x=>{
+          Swal.fire({ icon: 'info', title: 'Grupo registrado correctamente', text: x.mensaje, });
+          this.grupo = {  
+            descripcion: "",
+            ubigeo:{
+              idUbigeo:-1,
+              departamento:"-1",
+              provincia:"-1",
+              distrito:"",
+            },
+            usuarioLider: {
+              idUsuario: -1
+            },
+            usuarioRegistro: {
+                idUsuario: -1
+            },
+            usuarioActualiza: {
+                idUsuario: -1
+            },  
+            }
+        }
+      );
+
+
+
+}
+
+
+
+validaDescripcion(control: FormControl) {
+  console.log(">>> validaDescripcion [inicio] " + control.value);
+  
+   return this.grupoService.validaDescripcionRegistra(control.value).pipe(
+     map((resp: any) => { 
+          console.log(">>> validaDescripcion [resp] " + resp.valid);
+          return (resp.valid) ? null : {existeDescripcion: true} ;    
+        })
+    );
+    
+}
+
+listaProvincia(){
+  console.log("listaProvincia>>> " + this.grupo.ubigeo?.departamento);
+  this.utilService.listaProvincias(this.grupo.ubigeo?.departamento).subscribe(
+      x => this.provincias = x
+  );
+}
+
+listaDistrito(){
+  console.log("listaDistrito>>> " + this.grupo.ubigeo?.departamento);
+  console.log("listaDistrito>>> " + this.grupo.ubigeo?.provincia);
+  this.utilService.listaDistritos(this.grupo.ubigeo?.departamento,this.grupo.ubigeo?.provincia).subscribe(
+      x => this.distritos = x
+  );
+}
+
 
 }
