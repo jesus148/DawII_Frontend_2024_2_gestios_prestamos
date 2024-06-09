d1d7623a09049f6dbf0f2f87f991071d8539eaae
JuanRamosAnto <juanramosanto@gmail.com>
2024-06-08 21:33:45 -0500

    Registro MontoPrestamo

--- src/app/models/monto-prestamo.model.ts
+++ src/app/models/monto-prestamo.model.ts
@@ -1,2 +1,15 @@
+import { DataCatalogo } from "./dataCatalogo.model";
+import { Usuario } from "./usuario.model";
 export class MontoPrestamo {
+
+     idMontoPrestamo ?: number;
+     capital ?: number;
+     dias ?: DataCatalogo;
+	monto ?: number;
+	estado ?: number;
+     usuarioRegistro ?: Usuario;
+     usuarioActualizacion ?: Usuario;
+    
+    
+
 }
--- src/app/services/monto-prestamo.service.ts
+++ src/app/services/monto-prestamo.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MontoPrestamo } from '../models/monto-prestamo.model';
+import { Observable } from 'rxjs';
+import { AppSettings } from '../app.settings';
+
+const baseUrlPrueba = AppSettings.API_ENDPOINT+ '/montoPrestamo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MontoPrestamoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public registraMontoPrestamo(MontoPrestamo:MontoPrestamo) :Observable<any>{
+    return this.http.post(baseUrlPrueba, MontoPrestamo);
+  }
 }
