159e20510378ab3ddd50b9eba44af69f31c51294
jesus148 <jorgecop149@gmail.com>
2024-06-09 23:13:08 -0500

    jesus

--- src/app/models/monto-prestamo.model.ts
+++ src/app/models/monto-prestamo.model.ts
@@ -1,2 +1,27 @@
+import { DecimalPipe } from "@angular/common";
+import { DataCatalogo } from "./dataCatalogo.model";
+import { Usuario } from "./usuario.model";
+
 export class MontoPrestamo {
+
+
+  idMontoPrestamo?:Number;
+
+  capital?:number;
+  dias?: DataCatalogo;
+  monto?:DecimalPipe;
+  estado?: number;
+  fechaRegistro?:Date;
+  fechaActualizacion?:Date;
+  usuarioRegistro?:Usuario;
+  usuarioActualiza?:Usuario;
+
+
+
+
+
+
+
+
+
 }
--- src/app/services/solicitud-prestamo.service.ts
+++ src/app/services/solicitud-prestamo.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SolicitudPrestamo } from '../models/solicitud-prestamo.model';
+import { Observable } from 'rxjs';
+import { AppSettings } from '../app.settings';
+
+const baseUrlPrueba = AppSettings.API_ENDPOINT+ '/solicitudPrestamo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SolicitudPrestamoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  registrar(data:SolicitudPrestamo):Observable<any>{
+    return this.http.post(baseUrlPrueba, data);
+  }
+
+
+
+
+
 }
