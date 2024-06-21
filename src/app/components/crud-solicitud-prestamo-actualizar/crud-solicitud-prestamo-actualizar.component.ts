caebba2e24960724cec1b1a9b5458d35046742c8
JuanRamosAnto <juanramosanto@gmail.com>
2024-06-21 00:26:25 -0500

    PC2-Solicitus

--- src/app/components/crud-solicitud-prestamo-actualizar/crud-solicitud-prestamo-actualizar.component.ts
+++ src/app/components/crud-solicitud-prestamo-actualizar/crud-solicitud-prestamo-actualizar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { AppMaterialModule } from '../../app.material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from '../../menu/menu.component';
 
 @Component({
   selector: 'app-crud-solicitud-prestamo-actualizar',
   standalone: true,
-  imports: [],
+  imports: [AppMaterialModule, FormsModule, CommonModule, MenuComponent, ReactiveFormsModule],
   templateUrl: './crud-solicitud-prestamo-actualizar.component.html',
   styleUrl: './crud-solicitud-prestamo-actualizar.component.css'
 })
--- src/app/components/crud-solicitud-prestamo-agregar/crud-solicitud-prestamo-agregar.component.ts
+++ src/app/components/crud-solicitud-prestamo-agregar/crud-solicitud-prestamo-agregar.component.ts
@@ -1,12 +1,81 @@
 import { Component } from '@angular/core';
+import { AppMaterialModule } from '../../app.material.module';
+import { FormsModule, FormBuilder, Validators, ReactiveFormsModule, Form, FormControl } from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from '../../menu/menu.component';
+import {DataCatalogo} from '../../models/dataCatalogo.model';
+import {SolicitudPrestamo} from '../../models/solicitud-prestamo.model'
+import { MontoPrestamo } from '../../models/monto-prestamo.model';
+import Swal from 'sweetalert2';  //importando para los mensajes
+import { UtilService } from '../../services/util.service';
+import { Usuario } from '../../models/usuario.model';
+import { TokenService } from '../../security/token.service';
+import { SolicitudPrestamoService } from '../../services/solicitud-prestamo.service';
+
+
 
 @Component({
   selector: 'app-crud-solicitud-prestamo-agregar',
   standalone: true,
-  imports: [],
+  imports: [AppMaterialModule, FormsModule, CommonModule, MenuComponent, ReactiveFormsModule],
   templateUrl: './crud-solicitud-prestamo-agregar.component.html',
   styleUrl: './crud-solicitud-prestamo-agregar.component.css'
 })
 export class CrudSolicitudPrestamoAgregarComponent {
+  
+  // GLOBALES PARA COMBO Y REGISTRAR
+// combo Dias prestamo
+lstDias: DataCatalogo[] = [];
+// combo capital
+  capital: MontoPrestamo[] = [];
+  // combo monto
+  monto: MontoPrestamo[] = [];
+  //lista de usuarios prestarios
+  lstPrestatarios: Usuario[] = [];
+  //usuario en sesion
+  objUsuario: Usuario = {};
+
+  // CARGANDO DATA PARA REGISTRAR
+  objPrestamo: SolicitudPrestamo = {
+    // dias
+    dias: {
+      idDataCatalogo: -1,
+     },
+  // capital
+      capital: 0.0,
+      montoPagar:0,
+      // data string desde el front conertimos a date
+      fechaInicioPrestamo: " ",
+      fechaFinPrestamo: " ",
+      usuarioPrestatario: {
+          idUsuario: -1
+      },
+      usuarioRegistro: {
+          idUsuario: -1
+      },
+      usuarioActualiza: {
+          idUsuario: -1
+      },
+
+   };
+
+  // VALIDACIONES
+/*
+   formRegistrar = this.formBuilder.group({
+    validaDescripcion: ['', [Validators.required]],
+    validaDescripcion1: ['', [Validators.required]],
+    validaDescripcion2: ['', [Validators.required]],
+    validaPais: ['', [Validators.min(1)]],
+    validaCapital: ['', [Validators.min(1)]],
+    validaUsuario: ['', [Validators.min(1)]],
+
+});
+*/
+
 
 }
